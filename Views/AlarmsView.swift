dbc95907ee9ce1c56cf57202756e9be9404759df
Nathan Roett <Nroett@lcs.on.ca>
2026-02-23 14:42:15 -0500

    Clock part 2

---
+++ Views/AlarmsView.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmsView.swift
+//  Clock
+//
+//  Created by Nathan Roett on 2/23/26.
+//
+
+import SwiftUI
+
+struct AlarmsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlarmsView()
+}
---
+++ Views/WorldClockView.swift
@@ -0,0 +1,18 @@
+//
+//  WorldClockView.swift
+//  Clock
+//
+//  Created by Nathan Roett on 2/23/26.
+//
+
+import SwiftUI
+
+struct WorldClockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorldClockView()
+}
