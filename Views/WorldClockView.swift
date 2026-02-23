0efe20c359af2d05bc5377cafdec5f4557566380
Nathan Roett <Nroett@lcs.on.ca>
2026-02-23 15:23:02 -0500

    Clock app part 4

--- Views/WorldClockView.swift
+++ Views/WorldClockView.swift
@@ -9,11 +9,80 @@ import SwiftUI
 
 struct WorldClockView: View {
     var body: some View {
-        Text("World Clock")
+        NavigationStack {
+            VStack {
+                // Ottawa
+                ExtractedView()
+                HStack {
+                    // Left side
+                    VStack {
+                        Text("Today, +0HRS")
+                        Text("Ottawa")
+                            .font(.system(.largeTitle, design: .default, weight: .thin))
+                    }
+
+                    Spacer()
+
+                    // Right side
+                    Text("6:35")
+                        .font(.system(size: 64.0, weight: .thin, design: .default))
+                    Text("AM")
+                        .font(.system(.largeTitle, design: .default, weight: .thin))
+                }
+
+                // Vancouver
+                HStack {
+                    // Left side
+                    VStack {
+                        Text("Today, -3HRS")
+                        Text("Vancouver")
+                            .font(.system(.largeTitle, design: .default, weight: .thin))
+                    }
+
+                    Spacer()
+
+                    // Right side
+                    Text("3:35")
+                        .font(.system(size: 64.0, weight: .thin, design: .default))
+                    Text("AM")
+                        .font(.system(.largeTitle, design: .default, weight: .thin))
+                }
+            }
             .navigationTitle("World Clock")
+            .toolbar {
+                // ...
+            }
+        }
     }
 }
 
 #Preview {
-    LandingView()
+        LandingView()
+    }
+
+struct ExtractedView: View {
+    
+    // MARK: Stored properties
+    let timeZorneOffset: String
+    let city: String
+    let time: String
+    let amOrPm: String
+    
+    var body: some View {
+        //left side
+        VStack {
+            Text("Today, +0HRS")
+            Text("Ottawa")
+                .font(.system( .largeTitle, design: .default, weight: .thin))
+        }
+        
+        Spacer()
+        
+        //Right side
+        Text("6:35")
+            .font(.system(size: 64.0, weight: .thin, design: .default))
+        Text("AM")
+            .font(.system(.largeTitle, design: .default, weight: .thin))
+        }
+    }
 }
