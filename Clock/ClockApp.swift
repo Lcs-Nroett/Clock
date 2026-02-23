b3f141c33c64a569067b34868a56bf6aa3893ba2
Nathan Roett <Nroett@lcs.on.ca>
2026-02-23 14:20:51 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Nathan Roett on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
