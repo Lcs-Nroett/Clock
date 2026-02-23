9066b7b0e190d744898ecfdc530175820dbba3d5
Nathan Roett <Nroett@lcs.on.ca>
2026-02-23 14:50:45 -0500

    Clock app part 3 :)

--- Views/WorldClockView.swift
+++ Views/WorldClockView.swift
@@ -9,10 +9,11 @@ import SwiftUI
 
 struct WorldClockView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("World Clock")
+            .navigationTitle("World Clock")
     }
 }
 
 #Preview {
-    WorldClockView()
+    LandingView()
 }
